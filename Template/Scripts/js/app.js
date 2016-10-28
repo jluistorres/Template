e128561ae32cd0cffd15a9d69bb73618d95a02a2
jluistorres <jtorreszarate2405@gmail.com>
2016-10-28 17:17:05 -0500

    Primer commit

---
+++ Template/Scripts/js/app.js
@@ -0,0 +1,6 @@
+﻿var AppMaster = {};
+toastr.options = { "progressBar": false, "positionClass": "toast-bottom-right", "closeButton": true, "showMethod": "slideDown" };
+
+$(function () {
+    AppTheme.init();
+});
