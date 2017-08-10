00e59da6d56cb3537242aa439c0c11cbc6cf48cb
jluistorres <jtorreszarate2405@gmail.com>
2017-08-10 15:10:59 -0500

    Sibebar menu selected script

--- Template/Scripts/js/init.layout.js
+++ Template/Scripts/js/init.layout.js
@@ -26,7 +26,7 @@ var AppLayout = function () {
             var bag = $(target).find('.dropdown-menu');
             bag.html('<li class="media"><a>Cargando...<a/></li>');
 
-            var url = bag.data('url') + 'BagList';            
+            var url = bag.data('url') + 'BagList';
             $.get(url).done(function (html) {
                 bag.html(html);
                 //Layout.scroller();
@@ -37,10 +37,10 @@ var AppLayout = function () {
 
                 //Escribir en una cookie para notificaciones
                 if ($(target).hasClass('notifications')) {
-                    Notificaciones.WriteCookie(countItems);                    
+                    Notificaciones.WriteCookie(countItems);
                 }
             });
-        });       
+        });
 
         //Eliminar item
         $('body').on('click', '.dropdown-list .media-delete', function () {
@@ -69,7 +69,7 @@ var AppLayout = function () {
             var action = cart.find('.dropdown-menu').data('url') + 'BagRemoveAll'; //$(this).data('action');
             var isNotificacion = $(this).closest('.dropdown-list').hasClass('notifications');
 
-            if (action) {                
+            if (action) {
                 swal({
                     title: 'Eliminar',
                     text: '¿Desea eliminar todos los elementos?',
@@ -118,10 +118,34 @@ var AppLayout = function () {
         //});
     }
 
+    var handleActiveSidebar = function () {
+        var items = $('.nav a');
+        var location = window.location.pathname;       
+
+        //Buscamos todas las coincidencias de la url actual en el nav sidebar
+        items.each(function (index, a) {
+            var href = $(a).attr('href');
+            if (href != '' && href == location) {
+                $(a).parent().addClass('active');                
+
+                var el = $(a).parent();
+                do {
+                    if (el.hasClass('sub-menu')) el.show();
+                    else if (el.hasClass('has-sub')) el.addClass('expand');
+                    el = el.parent();                    
+                } while (el.hasClass('has-sub') || el.hasClass('sub-menu'));
+
+                //Retornamos si encontramos la primera coincidencia
+                return !1;
+            }
+        });
+    }
+
     return {
         init: function () {
             handleBag();
             handlePropagation();
+            handleActiveSidebar();
         }
     };
 }();
